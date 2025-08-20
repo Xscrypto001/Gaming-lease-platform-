826ad92725caa9e5f7935bcc0b7c19aec2d599c2
xscrypto001 <ikinyapeter93@gmail.com>
2025-08-20 13:29:54 +0300

    feat: Admin panel fix

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
+
+
+
+
+admin.site.register(Profile)
+admin.site.register(User)
+admin.site.register(Item)
+admin.site.register(Transaction)
+
 
 # Register your models here.
